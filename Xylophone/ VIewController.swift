ca039a77791b0fb2f197d984255592d9202497ae
chrisjwwalker <cjw@walker54.plus.com>
2018-07-06 09:25:34 +0100

    Xylophone app

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
-
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
+        playNote(key: sender.tag)
+        animate(sender: sender)
     }
     
-  
-
+    private func playNote(key: Int) {
+        let soundUrl = Bundle.main.url(forResource: "note\(key)", withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundUrl!)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        audioPlayer.play()
+    }
+    
+    func animate(sender: UIButton) {
+        sender.transform = CGAffineTransform(scaleX: 0.2, y: 0.2)
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       usingSpringWithDamping: CGFloat(1.0),
+                       initialSpringVelocity: CGFloat(0.1),
+                       options: UIViewAnimationOptions.allowUserInteraction,
+                       animations: { sender.transform = CGAffineTransform.identity },
+                       completion: { Void in() })
+    }
 }
 
